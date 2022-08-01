2457cf700455a238d82db31adc2940203542ffdf
Nandini <nandini.sn585@gmail>
2022-08-01 16:51:14 +0200

    add few piece of code in the first program

---
+++ First_python_practise.py
@@ -0,0 +1,10 @@
+import math as mt
+
+def drop_first_last(grades):
+    first, *middle, last = grades
+    print(first)
+    print(middle)
+    print(last)
+    
+abc = [10,20,30,50,60,70,80]
+drop_first_last(abc)
